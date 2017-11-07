6c8b6fa4508ce5d2c725780f7bab29672e54588a
baver_t@hotmail.com <Ankara06>
2017-11-08 01:22:06 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FacebookService, LoginResponse, LoginOptions, UIResponse, UIParams, FBVideoComponent } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,119 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  @ViewChild(FBVideoComponent) video: FBVideoComponent;
+
+  constructor(
+    private fb: FacebookService
+  ) {
+
+    console.log('Initializing Facebook');
+
+    fb.init({
+      appId: '1927971220769787',
+      version: 'v2.9'
+    });
+
+  }
+
+  login() {
+    this.fb.login()
+      .then((res: LoginResponse) => {
+        console.log('Logged in', res);
+      })
+      .catch(this.handleError);
+  }
+
+  /**
+   * Login with additional permissions/options
+   */
+  loginWithOptions() {
+
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile,user_friends,email,pages_show_list'
+    };
+
+    this.fb.login(loginOptions)
+      .then((res: LoginResponse) => {
+        console.log('Logged in', res);
+      })
+      .catch(this.handleError);
+
+  }
+
+  getLoginStatus() {
+    this.fb.getLoginStatus()
+      .then(console.log.bind(console))
+      .catch(console.error.bind(console));
+  }
+
+
+  /**
+   * Get the user's profile
+   */
+  getProfile() {
+    this.fb.api('/me')
+      .then((res: any) => {
+        console.log('Got the users profile', res);
+      })
+      .catch(this.handleError);
+  }
+
+
+  /**
+   * Get the users friends
+   */
+  getFriends() {
+    this.fb.api('/me/friends')
+      .then((res: any) => {
+        console.log('Got the users friends', res);
+      })
+      .catch(this.handleError);
+  }
+
+
+  /**
+   * Show the share dialog
+   */
+  share() {
+
+    const options: UIParams = {
+      method: 'share',
+      href: 'https://webrazzi.com/2017/11/07/389744/'
+    };
+
+    this.fb.ui(options)
+      .then((res: UIResponse) => {
+        console.log('Got the users profile', res);
+      })
+      .catch(this.handleError);
+
+  }
+
+
+  playVideo() {
+    this.video.play();
+  }
+
+  onVideoEvent(ev) {
+    console.log('Video event fired: ' + ev);
+  }
+
+  pauseVideo() {
+    this.video.pause();
+  }
+
+
+
+  /**
+   * This is a convenience method for the sake of this example project.
+   * Do not use this in production, it's better to handle errors separately.
+   * @param error
+   */
+  private handleError(error) {
+    console.error('Error processing action', error);
+  }
 }
