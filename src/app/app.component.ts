fb8312605e43c7dfb3136aaf38593c08911b3c81
serdarturkmen <baver_t@hotmail.com>
2017-11-08 10:11:42 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { FacebookService, LoginResponse, LoginOptions, UIResponse, UIParams, FBVideoComponent } from 'ngx-facebook';
+import { Component } from '@angular/core';
+import { FacebookService } from 'ngx-facebook';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +8,6 @@ import { FacebookService, LoginResponse, LoginOptions, UIResponse, UIParams, FBV
 })
 export class AppComponent {
   title = 'app';
-
-  @ViewChild(FBVideoComponent) video: FBVideoComponent;
 
   constructor(
     private fb: FacebookService
@@ -24,45 +22,6 @@ export class AppComponent {
 
   }
 
-
-
-  /**
-   * Login with additional permissions/options
-   */
-  loginWithOptions() {
-
-    const loginOptions: LoginOptions = {
-      enable_profile_selector: true,
-      return_scopes: true,
-      scope: 'public_profile,user_friends,email,pages_show_list'
-    };
-
-    this.fb.login(loginOptions)
-      .then((res: LoginResponse) => {
-        console.log('Logged in', res);
-      })
-      .catch(this.handleError);
-
-  }
-
-  /**
-   * Show the share dialog
-   */
-  share() {
-
-    const options: UIParams = {
-      method: 'share',
-      href: 'https://webrazzi.com/2017/11/07/389744/'
-    };
-
-    this.fb.ui(options)
-      .then((res: UIResponse) => {
-        console.log('Got the users profile', res);
-      })
-      .catch(this.handleError);
-
-  }
-  
   /**
    * This is a convenience method for the sake of this example project.
    * Do not use this in production, it's better to handle errors separately.
